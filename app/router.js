9ea5d31fdadd8e650aaec19b72199b7622ff6ede
ElodieFaivre <elodie.faivre3@gmail.com>
2021-12-08 14:11:39 +0100

    Atelier fini, reste le bonus

--- app/router.js
+++ app/router.js
@@ -2,47 +2,45 @@ const express = require("express");
 
 const router = express.Router();
 
-let isDiceRoller = false;
+//const games = require('./games.json');
+//server.locals.games = games;
 
-
+//router.use((req, res, next) => {
+//    req.isDiceRoller = false;
+//    next();
+//});
 
 router.get("/",(req,res)=>{
-    res.render('index', {
-        isDiceRoller
-    });
+    res.render('index');
 });
 
-router.get("/game/:nomDuJeu",(req,res)=>{
+router.get("/game/:nomDuJeu",(req,res, next)=>{
     const games = req.games;
     const game = games.find(game => game.name == req.params.nomDuJeu);
 
-    //if (game.name)
+    res.locals.isDiceRoller = false;
+
+    if (!game) {
+        console.log('########################### erreur #####################');
+        next();
+        return;
+    }
 
     if (game.name == 'diceRoller') {
-        isDiceRoller = true;
+        res.locals.isDiceRoller = true;
     }
-    else {
-        isDiceRoller = false;
-    }
-    res.render(game.name, {
-        isDiceRoller
-    });
+    //else {
+    //    isDiceRoller = false;
+    //}
+    res.render(game.name);
 });
 
 
-/*
-router.get("/game/fourchette",(req,res)=>{
-    res.render('fourchette', {
-        isDiceRoller
-    });
+
+
+
+router.use((req, res, next) => {
+    res.status(404).render('404');
 });
-
-router.get("/game/diceRoller",(req,res)=>{
-    res.render('diceRoller', {
-        isDiceRoller:true
-    });
-})
-*/
-
 
 module.exports = router;
